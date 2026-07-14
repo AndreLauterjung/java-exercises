15f7b3788e3a85e6df573334c8613b4b4893d9b1
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-14 20:43:20 -0300

    feat(01-comments): adiciona exercicio comentario linha

---
+++ 01-comments/exercise01/src/exercise01/Exercise01.java
@@ -0,0 +1,19 @@
+package exercise01;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise01
+{
+    public static void main(String[] args)
+    {
+        // ESTE É O COMENTÁRIO DE LINHA ÚNICA
+        // Começa com duas barras (//).
+        // Tudo o que estiver após as barras nesta linha é ignorado.
+        // Para que serve: Explicar linhas de código específicas ou desativar temporariamente uma linha.
+        
+        System.out.println("Exercício 1 - Comentário de linha."); // Mostra uma mensagem na tela.
+    }
+    
+}
