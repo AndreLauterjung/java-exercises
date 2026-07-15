b450119dca10069bcb883260b73fa190c8492839
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-14 21:32:39 -0300

    feat(01-comments): adiciona exercicio comentario bloco

---
+++ 01-comments/exercise02/src/exercise02/Exercise02.java
@@ -0,0 +1,15 @@
+package exercise02;
+
+public class Exercise02 
+{
+
+    public static void main(String[] args)
+    {
+        /* Comentário de bloco.
+        Tudo que estiver entre as barras faz parte do comentário
+        */
+        
+        System.out.println("Exercício 2 - Comentário de bloco.");
+    }
+    
+}
