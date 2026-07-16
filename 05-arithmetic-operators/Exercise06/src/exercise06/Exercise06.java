42e14a50733d00e6e20cece8a1c417b3b9d1b608
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 04:50:25 -0300

    feat(05-arithmetic-operators): adiciona exericio operador multiplicacao
    Enunciado: Determine o total de pães comprados em uma semana, sabendo que são comprados 6 por dia.

---
+++ 05-arithmetic-operators/Exercise06/src/exercise06/Exercise06.java
@@ -0,0 +1,24 @@
+package exercise06;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise06
+{
+
+    public static void main(String[] args)
+    {
+        
+        // Determine o total de pães comprados em uma semana, sabendo que são comprados 6 por dia.
+        
+        int paesDia = 6, diasSemana = 7, totalPaes;
+        
+        totalPaes = diasSemana * paesDia;
+        
+        System.out.println("São comprados "+totalPaes+" pães na semana!");
+        
+        
+    }
+    
+}
