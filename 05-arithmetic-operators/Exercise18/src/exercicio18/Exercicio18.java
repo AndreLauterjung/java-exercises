17746bd593154624a8bd6f929bfd2acb2af148ec
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 17:39:13 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador divisao
    Enunciado: Divida 50 balas entre 5 crianças.

---
+++ 05-arithmetic-operators/Exercise18/src/exercicio18/Exercicio18.java
@@ -0,0 +1,24 @@
+package exercicio18;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercicio18
+{
+    public static void main(String[] args)
+    {
+        // Divida 50 balas entre 5 crianças.
+        
+        int balas = 50, criancas = 5, balasCrianca;
+        
+        balasCrianca = balas / criancas;
+        
+        System.out.printf("Cada criança recebeu %d balas!\n", balasCrianca);
+        
+        
+        
+        
+    }
+    
+}
