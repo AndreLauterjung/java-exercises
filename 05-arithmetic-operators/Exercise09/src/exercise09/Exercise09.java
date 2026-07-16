1d868345e036b673e6efaccc7e95b13e56b1edb3
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 05:05:50 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador soma
    Enunciado: Some a pontuação de três rodadas de um jogo (ex: 15, 20 e 10 pontos).

---
+++ 05-arithmetic-operators/Exercise09/src/exercise09/Exercise09.java
@@ -0,0 +1,25 @@
+package exercise09;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise09
+{
+    public static void main(String[] args)
+    {
+        // Some a pontuação de três rodadas de um jogo (ex: 15, 20 e 10 pontos).
+        
+        int rodada1, rodada2, rodada3, totalPontos;
+        
+        rodada1 = 20;
+        rodada2 = 33;
+        rodada3 = 90;
+        
+        totalPontos = rodada1 + rodada2 + rodada3;
+        
+        System.out.println("Total de pontos conquistados na jogatina: "+totalPontos);
+        
+    }
+    
+}
