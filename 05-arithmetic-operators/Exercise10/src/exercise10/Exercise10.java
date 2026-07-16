25e92c86cc16338cb582c66b5515ce7513800c03
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 05:10:24 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador soma
    Enunciado: Calcule o perímetro de um quadrado de lado 5 (soma dos quatro lados).

---
+++ 05-arithmetic-operators/Exercise10/src/exercise10/Exercise10.java
@@ -0,0 +1,27 @@
+package exercise10;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise10
+{
+
+    public static void main(String[] args)
+    {
+        // Calcule o perímetro de um quadrado de lado 5 (soma dos quatro lados).
+        
+        int ladoA, ladoB, ladoC, ladoD, perimetro;
+        
+        ladoA = 5;
+        ladoB = 5;
+        ladoC = 5;
+        ladoD = 5;
+        
+        perimetro = ladoA + ladoB + ladoC + ladoD;
+        
+        System.out.println("O perímetro do quadrado de lado 5 é de: " + perimetro);
+        
+    }
+    
+}
