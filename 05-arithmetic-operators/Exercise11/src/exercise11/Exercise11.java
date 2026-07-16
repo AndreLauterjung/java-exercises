e50d446a23f14697c6daf2fd36768f1a3fa88b59
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 05:14:13 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador multiplicacao
    Enunciado: Multiplique a quantidade de caixas (10) pelo número de itens por caixa (12).

---
+++ 05-arithmetic-operators/Exercise11/src/exercise11/Exercise11.java
@@ -0,0 +1,22 @@
+package exercise11;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise11
+{
+    public static void main(String[] args)
+    {
+        // Multiplique a quantidade de caixas (10) pelo número de itens por caixa (12).
+        
+        int itensCaixa = 12, numCaixas = 10, totalItens;
+        
+        totalItens = numCaixas * itensCaixa;
+        
+        System.out.println("O total de itens é de: "+totalItens);
+        
+        
+    }
+    
+}
