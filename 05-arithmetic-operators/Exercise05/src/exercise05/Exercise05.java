923dd7840ced52543d3fe43ad555224638164948
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 03:54:45 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador subtracao
    Enunciado: Calcule a idade de uma pessoa subtraindo o ano de nascimento do ano atual (2024).

---
+++ 05-arithmetic-operators/Exercise05/src/exercise05/Exercise05.java
@@ -0,0 +1,24 @@
+package exercise05;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise05
+{
+
+    public static void main(String[] args)
+    {
+        // Calcule a idade de uma pessoa subtraindo o ano de nascimento do ano atual (2024).
+        
+        int actualYear = 2024, yearBorn, resultSub;
+        
+        yearBorn = 1930;
+        
+        resultSub = actualYear - yearBorn;
+        
+        System.out.println("Result: "+resultSub+" years old.");
+        
+    }
+    
+}
