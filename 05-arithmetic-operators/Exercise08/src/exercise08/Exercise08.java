47abaeec0b9621787267f334df76ac2c3815ce06
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 05:01:35 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador multiplicacao
    Enunciado: Calcule o total de dias em 15 semanas (semanas * 7).

---
+++ 05-arithmetic-operators/Exercise08/src/exercise08/Exercise08.java
@@ -0,0 +1,22 @@
+package exercise08;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise08 {
+
+
+    public static void main(String[] args)
+    {
+        // Calcule o total de dias em 15 semanas (semanas * 7).
+        
+        int diasSemana = 7, semanas = 15, totalDiasSemanas;
+        
+        totalDiasSemanas = semanas * diasSemana;
+        
+        System.out.printf("Em %d semanas existem %d dias!\n", semanas, totalDiasSemanas);
+         
+    }
+    
+}
