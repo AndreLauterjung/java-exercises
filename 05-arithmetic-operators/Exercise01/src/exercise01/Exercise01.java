a7494a4de4280a14b7f1c3adfe78cc6e8a0d9bba
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 03:29:30 -0300

    feat(05-arithmetic-operators): adiciona exercício operador soma
    Enunciado do exercício: some duas idades e exiba o resultado

---
+++ 05-arithmetic-operators/Exercise01/src/exercise01/Exercise01.java
@@ -0,0 +1,21 @@
+package exercise01;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise01
+{
+
+    public static void main(String[] args)
+    {
+        int age1 = 23, age2 = 32, sumAge;
+       
+        sumAge = age1 + age2;
+        
+        System.out.println("The sum of ages is: "+sumAge);
+        
+      
+    }
+    
+}
