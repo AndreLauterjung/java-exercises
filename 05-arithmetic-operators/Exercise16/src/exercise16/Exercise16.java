e5bc7efa381b653252a6381dde0c1f3c113852aa
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 17:30:30 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador subtracao
    Enunciado: Calcule quanto resta de uma barra de chocolate de 200g após comer 50g.

---
+++ 05-arithmetic-operators/Exercise16/src/exercise16/Exercise16.java
@@ -0,0 +1,24 @@
+package exercise16;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise16
+{
+    public static void main(String[] args)
+    {
+        // Calcule quanto resta de uma barra de chocolate de 200g após comer 50g.
+        
+        int pesoBarraChocolate = 200, quantidadeComida = 50, pesoRestanteBarra;
+        
+        pesoRestanteBarra = pesoBarraChocolate - quantidadeComida;
+        
+        System.out.printf("Restam %d gramas de barra de chocolate!\n", pesoRestanteBarra);
+        
+        
+        
+        
+    }
+    
+}
