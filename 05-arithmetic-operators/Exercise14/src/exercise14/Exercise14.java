9421787921965671f2dee779773d04aa5da108db
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 17:22:16 -0300

    feat(05-arithmetic-operators): adiciona exercicio operador multiplicacao
    Enunciado: Calcule o total de minutos em 3 horas (horas * 60).

---
+++ 05-arithmetic-operators/Exercise14/src/exercise14/Exercise14.java
@@ -0,0 +1,21 @@
+package exercise14;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise14
+{
+    public static void main(String[] args)
+    {
+        // Calcule o total de minutos em 3 horas (horas * 60).
+        
+        int horas = 3, minutos = 60, minutosTotais;
+
+        minutosTotais = horas * minutos;
+        
+        System.out.println("O total de minutos em três horas é: "+minutosTotais);
+        
+    }
+    
+}
