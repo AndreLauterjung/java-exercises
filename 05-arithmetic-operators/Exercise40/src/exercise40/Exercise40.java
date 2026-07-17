02dfd39c9566ed04b647d1043a0561c091178872
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-16 21:35:40 -0300

    feat(05-arithmetic-operators): adiciona exercicio divisao casting
    Enunciado: Realize o casting manual: divida 10 por 3 e armazene o resultado em uma variável int, descartando as casas decimais.

---
+++ 05-arithmetic-operators/Exercise40/src/exercise40/Exercise40.java
@@ -0,0 +1,24 @@
+package exercise40;
+
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise40
+{
+
+    public static void main(String[] args)
+    {
+        // Realize o casting manual: divida 10 por 3 e armazene o resultado em uma variável int, descartando as casas decimais.
+    
+        int resultado1;
+        double resultado2;
+        
+        resultado1 = (int) 10/3;
+        resultado2 = (double) 10/3;
+        
+        System.out.println("Resultado divisão (int): "+resultado1);
+        System.out.println("Resultado divisão (double): "+resultado2);
+    }
+    
+}
