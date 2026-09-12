9a30d4cc5563ff62a1ff49858b690104e42db8d4
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-09-12 18:16:48 -0300

    feat(05-arithmetic-operators): adiciona exercicio calculo area
    Adicionei um exercício para calcular a área de um retângulo solicitando ao usuário o valor da base e da altura do retângulo.

---
+++ 05-arithmetic-operators/Exercise63/src/exercise63/Exercise63.java
@@ -0,0 +1,35 @@
+package exercise63;
+
+import java.util.Scanner;
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise63
+{
+    public static void main(String[] args)
+    {
+        /*
+        Escreva um código que receba a base e a alturade um retângulo, calcule 
+        sua área e exiba na tela. Fórmula: área=base X altura
+        */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        double base;
+        double altura;
+        
+        System.out.println("Calculando área de um retângulo.");
+        System.out.println("Digite o valor total da base do retângulo: ");
+        base = sc.nextDouble();
+        
+        System.out.println("Digite o valor total da altura do retângulo: ");
+        altura = sc.nextDouble();
+        
+        double area = base * altura;
+        
+        System.out.println("Área do retângulo: "+area);
+        
+    }
+    
+}
