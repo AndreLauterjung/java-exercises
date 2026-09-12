284970288049ebdc8980b10e9fa3ccba60e79638
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-09-12 18:08:43 -0300

    feat(05-arithmetic-operators): adiciona exercicio calculo area
    Adicionei um exercício para calcular a área de um quadrado pedindo ao usuário o valor do lado do quadrado.

---
+++ 05-arithmetic-operators/Exercise62/src/exercise62/Exercise62.java
@@ -0,0 +1,31 @@
+package exercise62;
+
+import java.util.Scanner;
+        
+/**
+ *
+ * @author andrelauterjung
+ */
+public class Exercise62
+{
+
+    public static void main(String[] args)
+    {
+        /*
+        Escreva um código que receba o tamanho do lado de um quadrado, calcule 
+        sua área e exiba na tela. fórmula: área=lado X lado
+        */
+        
+        Scanner sc = new Scanner(System.in);
+        double lado;
+        
+        System.out.println("Digite o tamanho do lado do quadrado: ");
+        lado = sc.nextDouble();
+        
+        double area = lado * lado;
+        
+        System.out.println("Área do quadrado: "+area);
+        
+    }
+    
+}
